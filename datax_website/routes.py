141aaf784d6a53cebdc41c9b30cc8acd58124bdd
Loi Chii Lek <loichiilek@gmail.com>
2019-04-06 14:59:40 -0700

    updated required field

--- datax_website/routes.py
+++ datax_website/routes.py
@@ -1,8 +1,16 @@
 from datax_website import app
-from flask import render_template, url_for
+from flask import render_template, url_for, request, redirect
 
 
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template('home.html')
+
+
+@app.route("/query_wait_time", methods=['POST'])
+def query_wait_time():
+    print(request.method)
+    print("datetime: " + request.form.get('datetime') + " type: " + str(type(request.form.get('datetime'))))
+    print("ride: " + request.form.get('ride') + " type: " + str(type(request.form.get('ride'))))
+    return redirect(url_for('home'))
